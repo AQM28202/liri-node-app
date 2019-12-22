e05fdb73fade89ff414069154e09ba4caccc5f99
Anthony McPherson <aqm28202@gmail.com>
2019-12-22 14:26:27 -0500

    update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,12 @@
-console.log('this is loaded');
-
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.bandsInTown = {
+  id: process.env.bandskey
+};
+
+exports.omdb = {
+  id: process.env.omdb
+}
